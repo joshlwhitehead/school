437a531a176a923f57bfdf2449ba8192c871c826
josh <whitehead.josh98@gmail.com>
2022-02-07 12:35:18 -0700

    d

--- grad.py
+++ grad.py
@@ -7,11 +7,11 @@ semLate = wage*40*4*4
 lateGradSal = wage*40*4*12
 estGradSal = 70000
 
-def current_2026():
+def current_lateGrad():
     return yearCurrent + 2*semCurrent + 2.5*estGradSal
 
 def lateGrad():
     return 3*lateGradSal + 2*semLate + .5*estGradSal
 
-print('full vs part: ',lateGrad()-current_2026())
+print('full vs part: ',lateGrad()-current_lateGrad())
 print('yearly wage: ',wage*40*4*12)
