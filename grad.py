610a8c8ff9d8b34fa6c3a7c356ca15866748745f
josh <whitehead.josh98@gmail.com>
2022-02-07 09:21:54 -0700

    d

--- grad.py
+++ grad.py
@@ -1,17 +1,17 @@
 
-wage = 19
+wage = 23
 yearCurrent = 25000
 semCurrent = wage*15*4*4
 gradLongMonths = 44
 semLate = wage*40*4*4
 lateGradSal = wage*40*4*12
-estGradSal = 60000
+estGradSal = 70000
 
 def current_2026():
-    return yearCurrent + 2*semCurrent + 1.5*estGradSal
+    return yearCurrent + 2*semCurrent + 2.5*estGradSal
 
 def lateGrad():
-    return 2*lateGradSal + 2*semLate + 1.5*estGradSal
+    return 3*lateGradSal + 2*semLate + .5*estGradSal
 
-print(current_2026()-lateGrad())
-# print(lateGradSal)
+print(lateGrad()-current_2026())
+print(wage*40*4*12)
