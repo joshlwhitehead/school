4781d73571345846e3047dfe4b1c1bb01bad45b7
josh <whitehead.josh98@gmail.com>
2022-02-14 14:02:40 -0700

    d

--- grad.py
+++ grad.py
@@ -1,17 +1,32 @@
-wage = 25
+import numpy as np
+import matplotlib.pyplot as plt
 
-yearCurrent = 20000
-semCurrent = wage*15*4*4
-gradLongMonths = 44
-semLate = wage*40*4*4
-lateGradYear = wage*40*4*12
+wage = np.linspace(19,30)
+# yearsUntilGrad = np.linspace(1,5)
+
+
+fullYearAt19 = 25000
+monthDuringSem = wage*15*4*4
+fullYearAtNew = wage*40*4*12
 estGradSal = 70000
 
-def current_lateGrad(timeAfter,fullYearBefore,semesterCount):
-    return fullYearBefore*yearCurrent + semesterCount*semCurrent + timeAfter*estGradSal
+def shortGrad():
+    return fullYearAt19 + 2*monthDuringSem + 2.5*estGradSal
 
-def lateGrad(timeAfter,fullYearBefore,semesterCount):
-    return fullYearBefore*lateGradYear + semesterCount*semLate + timeAfter*estGradSal
+def longGrad(yearsToGrad):
+    return yearsToGrad*fullYearAtNew + (4.5-yearsToGrad)*estGradSal
 
-print('\nyou will have',lateGrad(.5,3,2)-current_lateGrad(2.5,1,2),'more dollars \n')
-# print('yearly wage: ',wage*40*4*12)
+# print(shortGrad()-longGrad(4))
+# print(wage*40*4*12)
+
+plt.plot(wage,shortGrad(),label='current')
+plt.plot(wage,longGrad(4),label='possible')
+plt.legend()
+plt.grid()
+plt.show()
+# plt.plot(yearsUntilGrad,np.ones(len(yearsUntilGrad))*shortGrad(),label='current')
+# plt.plot(yearsUntilGrad,longGrad(yearsUntilGrad),label='possible')
+# plt.legend()
+# plt.grid()
+# plt.show()
+
